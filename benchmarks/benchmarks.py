fa1e8d7df104746beaf847be41eff3a39d969b6f
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-10-18 10:16:48 +0100

    Refresh results, and start minimal benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,41 +1,21 @@
 import scipp as sc
 
-#
-# class TimeSuite:
-#     """
-#     An example benchmark that times the performance of various kinds
-#     of iterating over dictionaries in Python.
-#     """
-#     def setup(self):
-#         self.d = {}
-#         for x in range(500):
-#             self.d[x] = None
-#
-#     def time_keys(self):
-#         for key in self.d.keys():
-#             pass
-#
-#     def time_iterkeys(self):
-#         for key in self.d.iterkeys():
-#             pass
-#
-#     def time_range(self):
-#         d = self.d
-#         for key in range(500):
-#             x = d[key]
-#
-#     def time_xrange(self):
-#         d = self.d
-#         for key in xrange(500):
-#             x = d[key]
-
-
-class ScippVariableSuite:
+class Scipp:
     """
     Benchmark creating, adding, and, removing from Variables.
     """
     def setup(self):
-        self.var = None
+        self.var1 = sc.array(dims=['x'], values=[1, 2, 3, 4, 5])
+        self.var2 = sc.array(dims=['x'], values=[1, 2, 3, 4, 5])
 
-    def time_creating_scalar_variable(self):
-        self.var = sc.scalar(1)
+    def time_shallow_copy(self):
+        self.var1 = self.var2
+
+    def time_deep_copy(self):
+        self.var1.copy()
+
+    def time_variable_inplace_operation(self):
+        self.var1 += self.var2
+
+    def time_variable_non_inplace_operation(self):
+        self.var1 + self.var2
